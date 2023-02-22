1f351cddaa61ec07c981b39000cd7edc2881fbc8
mpkelevra23 <mpkelevra23@gmail.com>
2023-02-22 13:55:24 +0400

    lesson 2

---
+++ controllers/ActivityController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\controllers;
+
+use app\base\BaseController;
+
+class ActivityController extends BaseController
+{
+    public function actionCreate() {
+
+    }
+
+}
---
+++ controllers/actions/ActivityCreateAction.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\controllers\actions;
+
+use yii\base\Action;
+
+class ActivityCreateAction extends Action
+{
+    public function run()
+    {
+        
+    }
+
+}
